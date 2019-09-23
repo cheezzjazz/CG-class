55afe8b4d8a6f7f46fde4b521134564719971a99
cheezzjazz <vban@naver.com>
2019-09-23 12:29:28 +0900

    box2dapplication_both gravity and pulley example

--- Box2DApplication/Box2DApplication/main.cpp
+++ Box2DApplication/Box2DApplication/main.cpp
@@ -10,6 +10,7 @@ int scr_height = 640;
 // world, bodies, shapes
 b2World* world;
 b2Body* ground;
+b2Body* box;
 b2PulleyJoint* m_joint;	// Pulley Joint
 b2Body* body1 = NULL;	// Body1 of Pulley joint 
 b2Body* body2 = NULL;	// Body2 of Pulley joint 
@@ -23,28 +24,15 @@ int32 positionIterations = 3;	//the number of iterations for adjusting the posit
 float32 g_hz = 60.0f;			//frequency
 float32 timeStep = 1.0f / g_hz;
 
-bool bstart = false;
-void Dokeyboard(unsigned char key, int x, int y)
+bool bstart = true;
+
+enum MyExample
 {
-	switch (key)
-	{
-	/*case 'w':
-		box->ApplyLinearImpulseToCenter(b2Vec2(0.0f, 4000.0f), true);
-		break;
-	case 'a':
-		box->SetLinearVelocity(b2Vec2(30.0f, 0.0f));
-		break;
-	case 'd':
-		box->ApplyLinearImpulseToCenter(b2Vec2(400.0f, 0.0f), true);
-		break;*/
-	case 's':
-		bstart = true;
-		break;
-	default:
-		break;
-	}
-	glutPostRedisplay();
-}
+	mygravity = 1, mypulley
+};
+
+int nExample = mygravity;
+
 
 void Render()
 {
@@ -80,97 +68,100 @@ void Render()
 		glEnd();
 		glPopMatrix();
 	}
-	
-	//Draw Pulleys
-	if (body1 != NULL && body2 != NULL)
+
+	switch (nExample)
 	{
+	case mygravity:
+		//Box
+		if (box != NULL)
+		{
+			//get position and angle by body1(box)
+			b2Vec2 pos = box->GetPosition();
+			float32 ang = box->GetAngle();
+
+			glMatrixMode(GL_MODELVIEW);
+			glPushMatrix();
+			glTranslatef(pos.x, pos.y, 0.0f);	// Translation
+			glRotatef(ang, 0.0f, 0.0f, 1.0f);	// Rotation
+			glColor3f(0.9f, 0.2f, 0.4f);		// Set color
+			
+			//Draw the polygon shape with 4 vertices
+			glBegin(GL_QUADS);
+			for (int i = 0; i < 4; i++) {
+				glVertex2f(boxshape.m_vertices[i].x, boxshape.m_vertices[i].y);
+			}
+			glEnd();
+			glPopMatrix();
+		}
+		break;
+	case mypulley:
 		//Draw Pulleys
-		//Body Box_1
-		b2Vec2 pos1 = body1->GetPosition();
-		float a1 = body1->GetAngle();
+		if (body1 != NULL && body2 != NULL)
+		{
+			//Draw Pulleys
+			//Body Box_1
+			b2Vec2 pos1 = body1->GetPosition();
+			float a1 = body1->GetAngle();
 
-		glMatrixMode(GL_MODELVIEW);
+			glMatrixMode(GL_MODELVIEW);
 
-		glPushMatrix();
-		glTranslatef(pos1.x, pos1.y, 0.0f);
-		glRotatef(a1, 0.0f, 0.0f, 1.0f);
-		glColor3f(0.1f, 0.8, 0.2f);
+			glPushMatrix();
+			glTranslatef(pos1.x, pos1.y, 0.0f);
+			glRotatef(a1, 0.0f, 0.0f, 1.0f);
+			glColor3f(0.1f, 0.8, 0.2f);
 
-		glLineWidth(1.0f);
-		glBegin(GL_QUADS);
-		for (int i = 0; i < 4; i++) {
-			glVertex2f(boxshape.m_vertices[i].x, 
-				boxshape.m_vertices[i].y);
+			glLineWidth(1.0f);
+			glBegin(GL_QUADS);
+			for (int i = 0; i < 4; i++) {
+				glVertex2f(boxshape.m_vertices[i].x,
+					boxshape.m_vertices[i].y);
+			}
+			glEnd();
+			glPopMatrix();
+			//Body Box_2
+			b2Vec2 pos2 = body2->GetPosition();
+			float a2 = body2->GetAngle();
+
+			glMatrixMode(GL_MODELVIEW);
+
+			glPushMatrix();
+			glTranslatef(pos2.x, pos2.y, 0.0f);
+			glRotatef(a2, 0.0f, 0.0f, 1.0f);
+			glColor3f(0.1f, 0.8, 0.2f);
+
+			glLineWidth(1.0f);
+			glBegin(GL_QUADS);
+			for (int i = 0; i < 4; i++) {
+				glVertex2f(boxshape.m_vertices[i].x,
+					boxshape.m_vertices[i].y);
+			}
+			glEnd();
+			glPopMatrix();
+
+			//Draw the string
+			glMatrixMode(GL_MODELVIEW);
+			glPushMatrix();
+			glColor3f(0.8f, 0.8f, 0.8f);
+
+			glLineWidth(1.0f);
+			glBegin(GL_LINES);
+			glVertex2d(m_joint->GetAnchorA().x, m_joint->GetAnchorA().y);
+			glVertex2d(m_joint->GetGroundAnchorA().x, m_joint->GetGroundAnchorA().y);
+			glVertex2d(m_joint->GetGroundAnchorA().x, m_joint->GetGroundAnchorA().y);
+			glVertex2d(m_joint->GetGroundAnchorB().x, m_joint->GetGroundAnchorB().y);
+			glVertex2d(m_joint->GetGroundAnchorB().x, m_joint->GetGroundAnchorB().y);
+			glVertex2d(m_joint->GetAnchorB().x, m_joint->GetAnchorB().y);
+			glEnd();
+			glPopMatrix();
 		}
-		glEnd();
-		glPopMatrix();
-		//Body Box_2
-		b2Vec2 pos2 = body2->GetPosition();
-		float a2 = body2->GetAngle();
-
-		glMatrixMode(GL_MODELVIEW);
-
-		glPushMatrix();
-		glTranslatef(pos2.x, pos2.y, 0.0f);
-		glRotatef(a2, 0.0f, 0.0f, 1.0f);
-		glColor3f(0.1f, 0.8, 0.2f);
-
-		glLineWidth(1.0f);
-		glBegin(GL_QUADS);
-		for (int i = 0; i < 4; i++) {
-			glVertex2f(boxshape.m_vertices[i].x, 
-				boxshape.m_vertices[i].y);
-		}
-		glEnd();
-		glPopMatrix();
-
-		//Draw the string
-		glMatrixMode(GL_MODELVIEW);
-		glPushMatrix();
-		glColor3f(0.8f, 0.8f, 0.8f);
-
-		glLineWidth(1.0f);
-		glBegin(GL_LINES);
-		glVertex2d(m_joint->GetAnchorA().x, m_joint->GetAnchorA().y);
-		glVertex2d(m_joint->GetGroundAnchorA().x, m_joint->GetGroundAnchorA().y);
-		glVertex2d(m_joint->GetGroundAnchorA().x, m_joint->GetGroundAnchorA().y);
-		glVertex2d(m_joint->GetGroundAnchorB().x, m_joint->GetGroundAnchorB().y);
-		glVertex2d(m_joint->GetGroundAnchorB().x, m_joint->GetGroundAnchorB().y);
-		glVertex2d(m_joint->GetAnchorB().x, m_joint->GetAnchorB().y);
-		glEnd();
-		glPopMatrix();
+		break;
+	default:
+		break;
 	}
 	
 	glutSwapBuffers();
 }//end of Render()
 
-//
-//int prey = 0;
-//void Update(int value)
-//{
-//	if (bstart)
-//	{
-//		for (int i = 0; i < 2; i++)
-//		{
-//			world->Step(timeStep, velocityIterations, positionIterations);
-//			//Print
-//			b2Vec2 position = box->GetPosition();
-//	
-//			int y = (int)position.y;
-//			if (position.y >= 5.0f)
-//			{
-//				if (y != prey)
-//				{
-//					prey = y;
-//					std::cout << "Box position (" << position.x << ", " << y << ")" << std::endl;
-//				}
-//			}
-//			//
-//		}
-//	}	
-//	glutPostRedisplay();
-//	glutTimerFunc(20, Update, 0);
-//}
 
 void Update(int value)
 {
@@ -186,12 +177,6 @@ void Update(int value)
 	glutTimerFunc(20, Update, 0);	//Recursive function
 }
 
-//void close()
-//{
-//	std::cout << "!" << std::endl;
-//	delete world;
-//	world = NULL;
-//}
 
 void Reshape(int _width, int _height)
 {
@@ -217,8 +202,31 @@ void Setup()
 		ground->CreateFixture(&gnd_shape, 0.0f);
 	}
 
-	// Joint
+	switch (nExample)
 	{
+	case mygravity:
+	{
+		// Step1 : define body
+		b2BodyDef boxbd;
+		boxbd.type = b2_dynamicBody;
+		boxbd.position.Set(0.0f, 30.0f);
+		// Step2 : create body
+		b2Body* body = world->CreateBody(&boxbd);
+		// Step3 : create shape
+		boxshape.SetAsBox(5.0f, 5.0f);
+		// Step4 : create Fixture
+		b2FixtureDef boxfd;
+		boxfd.shape = &boxshape;
+		boxfd.density = 1.0f;
+		boxfd.restitution = 0.0f;
+		// Step5 : Attach shape to body with fixture
+		body->CreateFixture(&boxfd);
+		box = body;
+	}
+		break;
+	case mypulley:
+	{
+		// Joint
 		// body _ box size
 		float32 box_w = 2.0f;
 		float32 box_h = 2.0f;
@@ -236,7 +244,7 @@ void Setup()
 		float distance = b2Distance(groundanchor1, groundanchor2);
 		if (length > distance)
 			Length = (length - distance) / 2;
-		
+
 		//create the shape of body
 		boxshape.SetAsBox(box_w, box_h);
 
@@ -251,15 +259,41 @@ void Setup()
 		body2 = world->CreateBody(&bd);
 		body2->CreateFixture(&boxshape, 0.4f); //set different density of body 2
 
-		// define and create pulleys joint
+											   // define and create pulleys joint
 		b2PulleyJointDef pulleyDef;
 		anchor1.Set(groundanchor1.x, groundanchor1.y - Length);
 		anchor2.Set(groundanchor2.x, groundanchor2.y - Length);
 		pulleyDef.Initialize(body1, body2, groundanchor1, groundanchor2, anchor1, anchor2, 1.5f);
 		m_joint = (b2PulleyJoint*)world->CreateJoint(&pulleyDef);
 	}
+		break;
+	default:
+		break;
+	}
+	
 
 }//end Setup()
+
+void Dokeyboard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 'g':
+		nExample = mygravity;
+		Setup();
+		break;
+	case 'p':
+		nExample = mypulley;
+		Setup();
+		break;
+	case 's':
+		bstart = true;
+		break;
+	default:
+		break;
+	}
+	glutPostRedisplay();
+}
 
 int main(int argc, char** argv)
 {
